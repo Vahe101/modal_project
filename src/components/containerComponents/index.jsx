09bf3cd1d4c0f360371a6c8b5b71e76f9ffe0997
Vahe Davtyan <vahe.davtyan.200@gmail.com>
2021-02-22 18:27:22 +0400

    I changed the comments from @Shitikyan

--- src/components/containerComponents/index.jsx
+++ src/components/containerComponents/index.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ThemeContext from "../shared/Contexts";
 import DefaultContext from "../shared/Contexts/DefaultContext";
 import NameList from "../nameList";
 import Modal from "../shared/modal";
+import { data } from "../shared/Datas";
 
 const Container = () => {
-  const [names, setNames] = useState([{ key: 1, name: "Abgar", visible: true }, { key: 2, name: "Ashot", visible: true }, { key: 3, name: "Karen", visible: true }, { key: 4, name: "Petros", visible: true }, { key: 5, name: "Soxomon", visible: true }, { key: 6, name: "Abgar", visible: true }, { key: 7, name: "Ashot", visible: true }, { key: 8, name: "Karen", visible: true }, { key: 9, name: "Petros", visible: true }, { key: 10, name: "Soxomon", visible: true }, { key: 11, name: "Abgar", visible: true }, { key: 12, name: "Ashot", visible: true }, { key: 13, name: "Karen", visible: true }, { key: 14, name: "Petros", visible: true }, { key: 15, name: "Soxomon", visible: true }, { key: 16, name: "Abgar", visible: true }, { key: 17, name: "Ashot", visible: true }, { key: 18, name: "Karen", visible: true }, { key: 19, name: "Petros", visible: true }, { key: 20, name: "Soxomon", visible: true }]);
-  const [defaultNames, setDefaultNames] = useState([{ key: 1, name: "Abgar", visible: true }, { key: 2, name: "Ashot", visible: true }, { key: 3, name: "Karen", visible: true }, { key: 4, name: "Petros", visible: true }, { key: 5, name: "Soxomon", visible: true }, { key: 6, name: "Abgar", visible: true }, { key: 7, name: "Ashot", visible: true }, { key: 8, name: "Karen", visible: true }, { key: 9, name: "Petros", visible: true }, { key: 10, name: "Soxomon", visible: true }, { key: 11, name: "Abgar", visible: true }, { key: 12, name: "Ashot", visible: true }, { key: 13, name: "Karen", visible: true }, { key: 14, name: "Petros", visible: true }, { key: 15, name: "Soxomon", visible: true }, { key: 16, name: "Abgar", visible: true }, { key: 17, name: "Ashot", visible: true }, { key: 18, name: "Karen", visible: true }, { key: 19, name: "Petros", visible: true }, { key: 20, name: "Soxomon", visible: true }]);
+  const [names, setNames] = useState(data);
+  const [defaultNames, setDefaultNames] = useState(data);
 
   const value = {
     names: names,
@@ -17,12 +18,6 @@ const Container = () => {
     defaultNames: defaultNames,
     setDefaultNames: setDefaultNames,
   }
-
-  // useEffect(() => {
-  //   if (defaultNames.length === 0) {
-  //     setDefaultNames([...names]);
-  //   };
-  // }, []);
 
   return (
     <ThemeContext.Provider value={value}>
